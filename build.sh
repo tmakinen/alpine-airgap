4f366722d3bd649194843cffdd49da07688b3128
Timo Mäkinen <tmakinen@foo.sh>
2026-05-30 20:14:11 +0300

    Add airgap user and crypted /workspace mount

--- build.sh
+++ build.sh
@@ -34,11 +34,53 @@ ln -sf "/etc/init.d/hostname" "${apkovl}/etc/runlevels/boot/hostname"
 echo 'KEYMAP="/usr/share/bkeymaps/fi/fi-winkeys.bmap.gz"' > "${apkovl}/etc/loadkmap"
 ln -sf "/etc/init.d/loadkmap" "${apkovl}/etc/runlevels/boot/loadkmap"
 
+tar zxvf "${tmpdir}"/iso/apks/x86_64/alpine-baselayout-data-*.apk -C "${apkovl}"\
+    etc/group \
+    etc/passwd \
+    etc/shadow \
+    2> /dev/null
+echo "airgap:x:1000:" >> "${apkovl}/etc/group"
+echo "airgap:x:1000:1000:Airgap User:/workspace:/bin/sh" >> "${apkovl}/etc/passwd"
+echo "airgap:::0:::::" >> "${apkovl}/etc/shadow"
+sed -i -e 's/^root:/root:\!/' "${apkovl}/etc/shadow"
+
+mkdir -p "${apkovl}/etc/doas.d" "${apkovl}/etc/profile.d"
+cat <<EOF > "${apkovl}/etc/doas.d/doas.conf"
+permit persist :airgap
+permit nopass airgap as root cmd poweroff
+permit nopass airgap as root cmd reboot
+EOF
+cat <<EOF > "${apkovl}/etc/profile.d/reboot.sh"
+alias poweroff="doas poweroff"
+alias reboot="doas reboot"
+EOF
+
+mkdir -p "${apkovl}/etc/local.d" "${apkovl}/etc/runlevels/default"
+cat <<"EOF" > "${apkovl}/etc/local.d/encrypted-workspace.start"
+#!/bin/sh
+set -eu
+umask 077
+dd if=/dev/zero of=/run/workspace.img bs=1M count=100
+_loopdev="$(losetup -f)"
+losetup "$_loopdev" /run/workspace.img
+dd if=/dev/urandom bs=32 count=1 2> /dev/null | cryptsetup open --type plain "$_loopdev" secure-workspace -d -
+mkfs.ext4 /dev/mapper/secure-workspace
+mkdir /workspace
+mount -t ext4 /dev/mapper/secure-workspace /workspace
+chown airgap:airgap /workspace
+chmod 0700 /workspace
+EOF
+ln -s "/etc/init.d/local" "${apkovl}/etc/runlevels/default/local"
+chmod +x "${apkovl}/etc/local.d/encrypted-workspace.start"
+
 mkdir -p "${apkovl}/etc/apk"
 cat <<EOF > "${apkovl}//etc/apk/world"
 alpine-base
 cdrkit
+cryptsetup
+doas
 dvd+rw-tools
+e2fsprogs
 kbd-bkeymaps
 linux-lts
 openssh-client
